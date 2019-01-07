abaebac472b1c2c77e1318609d159287d6bf699e
kyasararamrana <45551096+kyasararamrana@users.noreply.github.com>
2019-01-07 11:37:28 +0530

    adding meta tags in every individual page

--- application/views/home/links.php
+++ application/views/home/links.php
@@ -1,8 +1,4 @@
 <title>SRB | <?php echo $pageTitle; ?></title>
-<meta charset="utf-8">
-<meta http-equiv="X-UA-Compatible" content="IE=edge">
-<meta name="description" content="OneTech shop project">
-<meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="stylesheet" type="text/css" href="<?php echo base_url('assets/css/bootstrap4/bootstrap.min.css'); ?>">
 <link href="<?php echo base_url('assets/plugins/fontawesome-free-5.0.1/css/fontawesome-all.css'); ?>" rel="stylesheet" type="text/css">
 <link rel="stylesheet" type="text/css" href="<?php echo base_url('assets/plugins/OwlCarousel2-2.2.1/owl.carousel.css'); ?>">
