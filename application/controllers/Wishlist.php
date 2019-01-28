fa04bbda6e852bd2ca4f8b465faab841c939cf29
kyasararamrana <45551096+kyasararamrana@users.noreply.github.com>
2019-01-28 12:39:30 +0530

    Deleting item in wishlist one item is added to cart

--- application/controllers/Wishlist.php
+++ application/controllers/Wishlist.php
@@ -11,6 +11,7 @@ class Wishlist extends CI_Controller
   {
     parent::__construct();
     $this->load->model('Wishlist_Model');
+    $this->load->model('Cart_Model');
   }
   //wishlist - (view)
   public function index()
@@ -69,6 +70,47 @@ class Wishlist extends CI_Controller
       } else {
         $this->session->set_flashdata('error','Please try again');
         redirect('wishlist');
+      }
+    } else {
+      $this->session->set_flashdata('error','Please login and try again');
+      redirect('home/login');
+    }
+  }
+  //insert cart
+  public function addtocart()
+  {
+    if($this->session->userdata('logged_in') == TRUE){
+      if ($this->input->post()) {
+        $post_data = array(
+          'user_id' => $this->input->post('user_id'),
+          'product_id' => $this->input->post('product_id'),
+          'product_name' => $this->input->post('product_name'),
+          'product_image' => $this->input->post('product_image'),
+          'actual_price' => $this->input->post('actual_price'),
+          'discount_percentage' => $this->input->post('discount_percentage'),
+          'net_price' => $this->input->post('net_price'),
+          'product_quantity' => $this->input->post('product_quantity')
+        );
+        $addl_data = array('created_on' => date('Y-m-d H:i:s'), 'created_by' => $this->session->userdata('id'));
+        $post_data = array_merge($post_data,$addl_data);
+        $id = $this->input->post('id');
+        if ($this->Cart_Model->insert($post_data) && $this->Wishlist_Model->delete($id)) {
+          $this->Wishlist_Model->delete($id);
+          $return['success'] = 'Item added to cart';
+          $user_id = $this->session->userdata('id');
+          if($user_id){
+            $return['count'] = $this->Cart_Model->get_cart_count_by_user_id($user_id)->cart_count;
+            $total_cart = $this->Cart_Model->get_cart_total_by_user_id($user_id)->total_cart;
+            $return['total'] = number_format($total_cart,2, '.', ',');
+          }
+          echo json_encode($return);
+        } else {
+          $return['error'] = 'Please try again';
+          echo json_encode($return);
+        }
+      } else {
+        $return['error'] = 'Please try again';
+        echo json_encode($return);
       }
     } else {
       $this->session->set_flashdata('error','Please login and try again');
--- application/views/home/wishlist.php
+++ application/views/home/wishlist.php
@@ -57,14 +57,15 @@
                                 <td class="price text-center"><strong>₹ <?php echo number_format($w->net_price,2,'.',','); ?></strong></td>
                               <?php } ?>
                               <td class="text-right">
-                                <a href="#" class="main-btn icon-btn addtocart"
+                                <a href="#" class="main-btn icon-btn addtocart_wishlist"
+                                data-id="<?php echo $w->id; ?>"
                                 data-product_id="<?php echo $w->product_id; ?>"
                                 data-product_name="<?php echo $w->product_name; ?>"
                                 data-product_image="<?php echo $w->product_image; ?>"
                                 data-actual_price="<?php echo $w->actual_price; ?>"
                                 data-discount_percentage="<?php echo $w->discount_percentage; ?>"
-                                data-net_price="<?php echo $w->net_price; ?>"
-                                ><i class="fa fa-cart-plus"></i></a> <a href="<?php echo base_url('wishlist/delete/'.$w->id); ?>" class="main-btn icon-btn"><i class="fa fa-trash"></i></a></td>
+                                data-net_price="<?php echo $w->net_price; ?>">
+                                <i class="fa fa-cart-plus"></i></a> <a href="<?php echo base_url('wishlist/delete/'.$w->id); ?>" class="main-btn icon-btn"><i class="fa fa-trash"></i></a></td>
                             </tr>
                           <?php } ?>
                         <?php } else { ?>
@@ -87,6 +88,43 @@
     </div>
     <?php echo $scripts ?>
     <script type="text/javascript">
+      $(document).ready(function(){
+        //add to Cart
+        $('.addtocart_wishlist').click(function(e){
+          e.preventDefault();
+          <?php if ($this->session->userdata('logged_in') != TRUE) { ?>
+            <?php $this->session->set_userdata('previous_url', current_url()); ?>
+            <?php echo 'window.location = "'.base_url('home/login').'";' ?>
+          <?php } else { ?>
+            var obj = $(this);
+            var id = obj.data('id');
+            var product_id = obj.data('product_id');
+            var product_name = obj.data('product_name');
+            var product_image = obj.data('product_image');
+            var actual_price = obj.data('actual_price');
+            var discount_percentage = obj.data('discount_percentage');
+            var net_price = obj.data('net_price');
+            var user_id = '<?php echo $this->session->userdata('id'); ?>';
+            var product_quantity = 1;
+            $.ajax({
+              url:'<?php echo base_url('wishlist/addtocart'); ?>',
+              type:'post',
+              data:{'id':id,'user_id':user_id,'product_id':product_id,'product_name':product_name,'product_image':product_image,'actual_price':actual_price,'discount_percentage':discount_percentage,'net_price':net_price,'product_quantity':product_quantity},
+              dataType:'json',
+              success:function(data){
+                if(data.success){
+                  $('.cart_count').find('span').html(data.count);
+                  $('.cart_price').html('₹ '+data.total);
+                  obj.parent().parent().remove();
+                  $('#message').html('<div class="alert_msg1 animated slideInUp bg-succ">'+data.success+' <i class="fa fa-check text-success ico_bac" aria-hidden="true"></i></div>');
+                } else {
+                  $('#message').html('<div class="alert_msg1 animated slideInUp bg-warn">'+data.error+' <i class="fa fa-exclamation-triangle text-success ico_bac" aria-hidden="true"></i></div>');
+                }
+              }
+            });
+          <?php } ?>
+        });
+      });
     </script>
   </body>
 </html>
