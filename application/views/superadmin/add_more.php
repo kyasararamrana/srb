3253b1e95c0ef3a48a2e868f98345bcb2c013557
SathwikGunda <42731745+SathwikGunda@users.noreply.github.com>
2019-02-19 16:27:03 +0530

    commit

---
+++ application/views/superadmin/add_more.php
@@ -0,0 +1,123 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>SRB |
+        <?php echo $pageTitle; ?>
+    </title>
+    <!-- Tell the browser to be responsive to screen width -->
+    <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
+    <?php echo $links; ?>
+</head>
+
+<body class="hold-transition skin-green sidebar-mini">
+    <div class="wrapper">
+
+        <?php echo $header; ?>
+        <?php echo $sidebar; ?>
+        <!-- Content Wrapper. Contains page content -->
+        <div class="content-wrapper">
+            <!-- Content Header (Page header) -->
+            <section class="content-header">
+                <h1>
+                    Add More
+                </h1>
+                <ol class="breadcrumb">
+                    <li><a href="#"><i class="fa fa-dashboard"></i>Dashboard</a></li>
+                    <li class="active">Add More</li>
+                </ol>
+            </section>
+
+            <!-- Main content -->
+            <section class="content container-fluid">
+
+                <div class="row">
+
+                    <div class="col-md-12">
+                        <div class="box box-success">
+                            <!-- form start -->
+                            <form id="" name="" action="" method="">
+                                <div class="box-body">
+                                    <div class="col-md-12">
+                                        <div class="row">
+                                            <div class="col-md-6">
+                                                <div class="form-group">
+                                                    <label>Text field</label>
+                                                    <input type="text" name="" placeholder="Text field" class="form-control" />
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div class="table-responsive">
+                                            <table id="myTable" class="table table-list">
+                                                <thead>
+                                                    <tr>
+                                                        <th>Text field</th>
+                                                        <th>&nbsp;</th>
+                                                    </tr>
+                                                </thead>
+                                                <tbody>
+                                                    <tr>
+                                                        <td>
+                                                            <input type="text" name="textfield" placeholder="Text Field" class="form-control" />
+                                                        </td>
+                                                        <td>&nbsp;</td>
+                                                    </tr>
+                                                </tbody>
+                                            </table>
+                                            <a href="#" class="btn btn-info btn-md mr-10">
+                                                <i class="fa fa-arrow-left mr-5"></i>Back
+                                            </a>
+                                            <button type="button" class="btn btn-md" id="addRow">Add Row</button>
+
+                                        </div>
+                                        <hr class="mb-10">
+                                        <div class="text-center">
+                                            <button type="submit" class="btn btn-md btn-success btn-flat">Submit</button>
+                                        </div>
+                                    </div>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+
+                </div>
+
+            </section>
+            <!-- /.content -->
+        </div>
+        <!-- /.content-wrapper -->
+        <?php echo $footer; ?>
+
+    </div>
+    <!-- ./wrapper -->
+
+    <?php echo $scripts; ?>
+
+    <script>
+        $(document).ready(function() {
+            var counter = 0;
+
+            $("#addRow").on("click", function() {
+                var newRow = $("<tr>");
+                var cols = "";
+
+                cols += '<td><input type="text" class="form-control" placeholder="Text Field" name="textfield' + counter + '"/></td>';
+
+                cols += '<td><button type="button" class="ibtnDel btn btn-md btn-danger"><i class="fa fa-trash"></i></button></td>';
+                newRow.append(cols);
+                $("table.table-list").append(newRow);
+                counter++;
+            });
+
+            $("table.table-list").on("click", ".ibtnDel", function(event) {
+                $(this).closest("tr").remove();
+                counter -= 1
+            });
+        });
+    </script>
+
+</body>
+
+</html>
