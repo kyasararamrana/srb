5d888a03988ea81788669bd3dd82a718e5307ece
vasudevareddy <vasudevareddy549@gmail.com>
2019-02-28 11:52:44 +0530

    delete  unwanted  data
    delete  unwanted  data

--- application/controllers/Vendor.php
+++ application/controllers/Vendor.php
@@ -64,7 +64,7 @@ class Vendor extends CI_Controller
     }
   }
  
-  //insert vendor
+  //insert/update vendor
   public function addpost()
   {
     if ($this->session->userdata('logged_in') == TRUE) {
@@ -114,7 +114,7 @@ class Vendor extends CI_Controller
       redirect('admin/login');
     }
   }
-  //roles list
+  //delete vendor
   public function deletes()
   {
     if ($this->session->userdata('logged_in') == TRUE) {
@@ -139,118 +139,10 @@ class Vendor extends CI_Controller
       redirect('admin/login');
     }
   }
-  //delete user
-  public function delete($id='')
-  {
-    if ($this->session->userdata('logged_in') == TRUE) {
-      if ($this->session->userdata('role') == 'Inventory') {
-        $post_data = array('status' => '0');
-        if ($this->Admin_Model->update($post_data,$id)) {
-          $this->session->set_flashdata('success','Role deleted successfully');
-          redirect('inventory/roles');
-        } else {
-          $this->session->set_flashdata('error','Please try again');
-          redirect('inventory/roles');
-        }
-      } else {
-        $this->session->set_flashdata('error','Sorry, you can\'t access');
-        redirect('admin');
-      }
-    } else {
-      $this->session->set_flashdata('error','Please login and try again');
-      redirect('admin/login');
-    }
-  }
-  //edit role - (view)
-  public function editrole($id='')
-  {
-    if ($this->session->userdata('logged_in') == TRUE) {
-      if ($this->session->userdata('role') == 'Inventory') {
-        $arg['pageTitle'] = 'Roles';
-        $data = components($arg);
-        $data['user'] = $this->Admin_Model->get_user_by_id($id);
-        $this->load->view('inventory/role',$data);
-      } else {
-        $this->session->set_flashdata('error','Sorry, you can\'t access');
-        redirect('admin');
-      }
-    } else {
-      $this->session->set_flashdata('error','Please login and try again');
-      redirect('admin/login');
-    }
-  }
-  //update
-  public function update()
-  {
-    if ($this->session->userdata('logged_in') == TRUE) {
-      if ($this->session->userdata('role') == 'Inventory') {
-        $post_data = $this->input->post();
-        if (isset($_FILES['image']['name']) && $_FILES['image']['name'] != '') {
-          $config['upload_path']   = './assets/uploads/admin/';
-          $config['allowed_types'] = 'gif|jpg|png';
-          $config['max_width']     = 128;
-          $config['max_height']    = 128;
-          $config['encrypt_name']  = TRUE;
-          $this->load->library('upload', $config);
-          if ( ! $this->upload->do_upload('image')) {
-            $error = $this->upload->display_errors();
-            $this->session->set_flashdata('error',$error);
-            redirect($this->agent->referrer());
-          } else {
-            $post_uploaded_image = $this->input->post('uploaded_image');
-            @unlink('./assets/uploads/admin/'.$post_uploaded_image);
-            $file_name = $this->upload->data('file_name');
-          }
-        }  else {
-          $file_name = $this->input->post('uploaded_image');
-        }
-        unset($post_data['uploaded_image']);
-        $addl_data = array(
-          'password' => md5($post_data['password']),
-          'org_password' => $post_data['confirmpassword'],
-          'image' => $file_name,
-          'updated_on' => date('Y-m-d H:i:s'),
-          'updated_by' => $this->session->userdata('id'),
-          'status' => 1
-        );
-        $post_data = array_merge($post_data,$addl_data);
-        unset($post_data['confirmpassword']);
-        if ($post_data) {
-          $post_id = $this->input->post('id');
-          if ($this->Admin_Model->update($post_data,$post_id)) {
-            $this->session->set_flashdata('success','Role updated successfully');
-            redirect('inventory/roles');
-          } else {
-            $this->session->userdata('error','Please try again');
-            redirect($this->agent->referrer());
-          }
-        } else {
-          $this->session->userdata('error','Please try again');
-          redirect($this->agent->referrer());
-        }
-      } else {
-        $this->session->set_flashdata('error','Sorry, you can\'t access');
-        redirect('admin');
-      }
-    } else {
-      $this->session->set_flashdata('error','Please login and try again');
-      redirect('admin/login');
-    }
-  }
-  //check email exists
-  public function check_exists()
-  {
-    $email = $this->input->post('email');
-    $id = $this->input->post('id');
-    if ($this->Admin_Model->check_exists($email,$id)) {
-      $isAvailable = false;
-    } else {
-      $isAvailable = true;
-    }
-    echo json_encode(array(
-      'valid' => $isAvailable,
-    ));
-  }
+  
+ 
+  
+  
  
   
 }
